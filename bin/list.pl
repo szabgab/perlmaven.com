1335f403b898129cc93d09d56916a0dab76d3041
Gabor Szabo <szabgab@gmail.com>
2013-04-16 22:53:29 +0300

    check for =original and for =translator entries

--- bin/list.pl
+++ bin/list.pl
@@ -34,3 +34,40 @@ foreach my $lang (@languages) {
 	say "   $_" for @drafts;
 }
 
+# collect all the english pages
+say "\nList of PAGES";
+say '-' x 30;
+my %META_PAGE = map { $_ => 1 } qw(index.tt about.tt keywords.tt archive.tt products.tt);
+{
+	my %english = map { substr(basename($_), 0, -3), 1 } glob "$root/sites/en/pages/*.tt";
+	foreach my $lang (@languages) {
+		next if $lang eq 'en';
+		my @pages = glob "$root/sites/$lang/pages/*.tt";
+		next if not @pages;
+		foreach my $file (@pages) {
+			next if $META_PAGE{ basename $file };
+			open my $fh, '<', $file or die "Could not open '$file' $!";
+			my $original;
+			my $translator;
+			while (my $line = <$fh>) {
+				chomp $line;
+				if ($line =~ /^=original\s+(\S+)/) {
+					$original = $1;
+				}
+				if ($line =~ /^=translator\s+(\S+)/) {
+					$translator = $1;
+				}
+			}
+			close $fh;
+			if (not defined $original) {
+				printf "File %-100s does not have an =original entry\n", $file;
+			} elsif (not $english{$original}) {
+				printf "File %-100s has =original entry '%s' which does not exist in the English version\n", $file, $original;
+			}
+			if (not defined $translator) {
+				printf "File %-100s does not have a =translator entry\n", $file;
+			}
+		}
+	}
+}
+
