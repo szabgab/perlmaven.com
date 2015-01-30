3a55b4d33ac808486ee4c8b33cb2e3734f8ea93f
Gabor Szabo <gabor@szabgab.com>
2015-01-30 09:01:32 +0200

    merge files article

---
+++ examples/merge_files.pl
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+
+my $target = pop @ARGV;
+my @sources = @ARGV;
+
+die "Usage: $0 in in ... in  out\n" if not @sources;
+
+open my $out, '>>', $target or die "Could not open '$target' for appending\n"; 
+foreach my $file (@sources) {
+    if (open my $in, '<', $file) {
+        while (my $line = <$in>) {
+            print $out $line;
+        }
+    } else {
+        warn "Could not open '$file' for reading\n";
+    }
+}
