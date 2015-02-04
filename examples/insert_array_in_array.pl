0e79cf0f3c7bf367921615cd3b4709660046d0db
Gabor Szabo <gabor@szabgab.com>
2015-02-04 19:44:25 +0200

    Fix examples

--- examples/insert_array_in_array.pl
+++ examples/insert_array_in_array.pl
@@ -6,6 +6,6 @@ use Data::Dumper;
 my @names = qw(Foo Bar Baz);
 my @languages = qw(Perl Python Ruby PHP);
 
-splice @names, 1, 1, @languages;
+splice @names, 1, 0, @languages;
 
 print Dumper \@names;
