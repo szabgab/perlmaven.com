07cf06d08eaaee9a93c32d5dca40422567549e7c
Gabor Szabo <gabor@szabgab.com>
2015-02-11 12:00:46 +0200

    weighted

---
+++ examples/weighted_no_replacement.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+use 5.010;
+
+use List::Util qw(sum);
+
+my @items = qw(low mid hight);
+my @weights = (10, 100, 1000);
+
+my $N = 10;
+
+foreach (1 .. $N) {
+    my $total = sum @weights;
+    say $total;
+    
+    my $rand = int rand $total;
+    say $rand;
+    my $selected = 0;
+    my $limit = $weights[$selected];
+    while ($rand >= $limit) {
+        $selected++;
+        $limit += $weights[$selected];
+    }
+    say "Selected: $items[$selected]";
+
+    $weights[$selected]--;
+}
+
+
