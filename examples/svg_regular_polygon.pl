007c83de59a6476a8cdc0eaa879bfded93bc5b5c
Gabor Szabo <gabor@szabgab.com>
2015-03-06 18:26:35 +0200

    start polygon article

---
+++ examples/svg_regular_polygon.pl
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+use SVG;
+use Math::Trig qw(:pi);
+
+print create() unless caller();
+sub create {
+    my $svg = SVG->new(
+        width  => 120,
+        height => 120,
+    );
+
+    my $n = 9;       # number of nodes
+    my $r = 50;      # radius
+    my $sr = 0;      # starting degree in radians
+    my $cx = 60;     # center x 
+    my $cy = 60;     # center y
+    my @x;
+    my @y;
+    for my $i (1 .. $n) {
+        push @x, $cx + $r * sin($sr + ($i * pi2 / $n)); 
+        push @y, $cy + $r * cos($sr + ($i * pi2 / $n));
+    }
+    my $lt = $svg->polygon(
+        %{ $svg->get_path(
+            x => \@x,
+            y => \@y,
+            -type   => 'polygon',
+            -closed  => 'true'
+        )},
+        style => {
+            'fill-opacity' => 1,
+            'fill'         => '#FF0000',
+            'stroke'       => '#999',
+            'stroke-width' => 1,
+        },
+    );
+
+    return $svg->xmlify;
+}
+
+1;
