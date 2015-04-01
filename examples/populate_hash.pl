6004a06c6ffdbb430f2e5642735d6571f77d9964
Gabor Szabo <gabor@szabgab.com>
2015-04-01 20:15:01 +0300

    3 examples

---
+++ examples/populate_hash.pl
@@ -0,0 +1,37 @@
+use strict;
+use warnings;
+use Data::Dumper;
+
+my $dir = shift or die "Usage: $0 DIR\n";
+
+my %data;
+collect_data();
+
+print Dumper \%data;
+
+sub collect_data {
+    opendir my $dh_root, $dir or die "Could not open '$dir'\n";;
+    foreach my $project (readdir $dh_root) {
+        next if $project eq '.' or $project eq '..';
+        print "$project\n";
+        opendir my $dh_project, "$dir/$project" or die "Could not open '$dir/$project'\n";
+        foreach my $library (readdir $dh_project) {
+            next if $library eq '.' or $library eq '..';
+            print "    $library\n";
+            opendir my $dh_library, "$dir/$project/$library" or die "Could not open '$dir/$project/$library'\n";
+            foreach my $type (readdir $dh_library) {
+                next if $type eq '.' or $type eq '..';
+                print "        $type\n"; 
+                opendir my $dh_type, "$dir/$project/$library/$type" or die "Could not open '$dir/$project/$library/$type'\n";
+                foreach my $file (readdir $dh_type) {
+                     next if $file eq '.' or $file eq '..';
+                     print "            $file\n";
+                     push @{ $data{$project}{$library}{$type} }, $file;
+                } 
+            }
+        }
+    }
+    return;
+}
+
+
