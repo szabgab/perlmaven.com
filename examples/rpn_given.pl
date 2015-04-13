f802813a847f48963ddb1c610f9cc198e5a36e45
Gabor Szabo <gabor@szabgab.com>
2015-04-13 17:40:27 +0300

    rpn examples

---
+++ examples/rpn_given.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use v5.10;
+no warnings 'experimental';
+
+my @stack;
+while (1) {
+
+    print '$ ';
+    my $in = <STDIN>;
+    chomp $in;
+
+    given ($in) {
+        when ("q") { last; }
+        when ("c") { 
+            pop @stack; 
+            }   # fetch the last value
+        when ("*") { 
+            my $x = pop(@stack);
+            my $y = pop(@stack);
+            push(@stack, $x*$y);
+            }
+        when ("+") { 
+            my $x = pop(@stack);
+            my $y = pop(@stack);
+            push(@stack, $x + $y);
+            }
+        when ("/") {
+            my $x = pop(@stack);
+            my $y = pop(@stack);
+            push(@stack, $y /  $x); 
+            }
+        when ("-") { 
+            my $x = pop(@stack);
+            my $y = pop(@stack);
+            push(@stack, $y - $x);
+            }
+        when ("=") { 
+            print pop(@stack), "\n"; 
+            }
+        default { push @stack, $in; }
+    }
+}
+
