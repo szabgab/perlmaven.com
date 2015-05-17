d8540d7d7b19fb517eaa941464c7c5cdf9720bac
Gabor Szabo <gabor@szabgab.com>
2015-05-17 09:47:40 +0300

    examples

---
+++ examples/js/node_underscore.js
@@ -0,0 +1,7 @@
+var _ = require('underscore');
+
+
+var numbers = [1, 2, 5, -3];
+var squares = _.map(numbers, function(x) { return x*x; });
+console.log(squares);
+
---
+++ examples/js/web_underscore.js
@@ -0,0 +1,4 @@
+var numbers = [1, 2, 5, -3];
+var squares = _.map(numbers, function(x) { return x*x; });
+console.log(squares);
+
