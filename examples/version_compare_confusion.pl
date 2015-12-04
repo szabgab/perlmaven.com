021a6c843753fddcc7602977ccfb9bfb92639730
Gabor Szabo <gabor@szabgab.com>
2015-12-04 16:15:07 +0200

    update

--- examples/version_compare_confusion.pl
+++ examples/version_compare_confusion.pl
@@ -7,3 +7,11 @@ use version;
 
 say Perl::Version->new('5.11') == Perl::Version->new('v5.11');   # 1
 say version->parse('5.11') == version->parse('v5.11');           #
+
+
+say Perl::Version->new('5.011') == Perl::Version->new('v5.11');   # 1
+say version->parse('5.011') == version->parse('v5.11');           # 1
+
+say Perl::Version->new('5.011') == Perl::Version->new('5.11');   # 1
+say version->parse('5.011') == version->parse('5.11');           # 0
+
