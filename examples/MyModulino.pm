1886c8defd11e793f90bd154d86e2a7337fb41f7
Gabor Szabo <gabor@szabgab.com>
2016-12-28 09:28:14 +0200

    add examples

---
+++ examples/MyModulino.pm
@@ -0,0 +1,14 @@
+package MyModulino;
+use 5.010;
+use strict;
+use warnings;
+
+hi() if not caller();
+ 
+sub hi {
+	say "Hello from main() of modulino.pl";
+}
+
+1;
+
+
---
+++ examples/use_my_modulino.pl
@@ -0,0 +1,9 @@
+use 5.010;
+use strict;
+use warnings;
+use MyModulino;
+
+say "before";
+MyModulino::hi();
+say "after";
+
