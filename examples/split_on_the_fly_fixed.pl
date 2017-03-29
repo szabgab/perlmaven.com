465a526e90affc42e7131a4aeee8453a8d0dd5c3
Gabor Szabo <gabor@szabgab.com>
2017-03-29 08:53:41 +0300

    another example

---
+++ examples/split_on_the_fly_fixed.pl
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+
+my $str = "root:*:0:0:System Administrator:/var/root:/bin/sh";
+my @indexes = (0, 4); ######## HERE####
+
+my ($username, $real_name) = (split /:/, $str)[@indexes]; ##and HERE##
+print "$username\n";
+print "$real_name\n";
+
