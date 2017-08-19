fd04550bf2c63717dc7808c8e9d4e74223792d7f
Gabor Szabo <gabor@szabgab.com>
2017-08-19 17:08:48 +0300

    more examples

---
+++ examples/regex/subst_func_e.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+use 5.010;
+
+sub xy_1 {
+    return 11;
+}
+sub ab_2 {
+    return 22;
+}
+
+my $text = 'some xy_1 ab_2';
+
+$text =~ s{(\w+_\d+)}{ "$1()"}eg;
+say $text;   # some xy_1() ab_2()
+
