b34e998396ffb53b58cf259f28b851f2a86d3fd9
Gabor Szabo <gabor@szabgab.com>
2018-03-02 16:24:25 +0200

    new draft

---
+++ examples/markua-parser/79f7a57fd459144a0720e99abeae7191a622ee1f/lib/Markua/Parser.pm
@@ -0,0 +1,30 @@
+package Markua::Parser;
+use strict;
+use warnings;
+use Path::Tiny qw(path);
+
+our $VERSION = 0.01;
+
+sub new {
+    my ($class) = @_;
+    my $self = bless {}, $class;
+    return $self;
+}
+
+sub parse_file {
+    my ($self, $filename) = @_;
+    my @entries;
+    for my $line (path($filename)->lines_utf8) {
+        if ($line =~ /^# (\S.*)/) {
+            push @entries, {
+                tag => 'h1',
+                text => $1,
+            };
+        }
+    }
+    return \@entries;
+}
+
+
+1;
+
