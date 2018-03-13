d3ab05ff45137a770bb3e74c6b0a6deba6f54a8c
Gabor Szabo <gabor@szabgab.com>
2018-03-13 22:25:48 +0200

    move code to file

---
+++ examples/script.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+=pod
+
+=head1 DESCRIPTION
+
+This script can have 2 parameters. The name or address of a machine
+and a command. It will execute the command on the given machine and
+print the output to the screen.
+
+=cut
+
+print "Here comes the code ... \n";
