a962be73ae1f22af43c2a55a4041445d6114b477
Gabor Szabo <gabor@szabgab.com>
2018-03-17 22:20:45 +0200

    extract code

---
+++ examples/recurse.pl
@@ -0,0 +1,23 @@
+use strict;
+use warnings;
+use 5.010;
+
+my $path = shift || '.';
+
+traverse($path);
+
+sub traverse {
+    my ($thing) = @_;
+	
+    say $thing;
+    return if not -d $thing;
+    opendir my $dh, $thing or die;
+    while (my $sub = readdir $dh) {
+        next if $sub eq '.' or $sub eq '..';
+
+        traverse("$thing/$sub");
+    }
+    close $dh;
+    return;
+}
+
---
+++ examples/tree.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use 5.010;
+
+my $path = shift || '.';
+
+traverse($path);
+
+sub traverse {
+    my ($thing) = @_;
+
+    return if not -d $thing;
+    opendir my $dh, $thing or die;
+    while (my $sub = readdir $dh) {
+        next if $sub eq '.' or $sub eq '..';
+        say "$thing/$sub";
+        traverse("$thing/$sub");
+    }
+    close $dh;
+    return;
+}
+
