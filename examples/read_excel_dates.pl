6b9f10e3eaf962fe6c92a277be72b16007277213
Gabor Szabo <gabor@szabgab.com>
2018-07-02 16:36:43 +0300

    add example

---
+++ examples/read_excel_dates.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use 5.010;
+
+my $file = shift or die "Usage: $0 FILENAME\n";
+
+use Spreadsheet::Read qw(ReadData);
+
+my $book = ReadData($file);
+
+say 'A1: ' . $book->[1]{A1}; 
+say 'A2: ' . $book->[1]{A2}; 
+say 'A3: ' . $book->[1]{A3}; 
+say 'A4: ' . $book->[1]{A4}; 
+
