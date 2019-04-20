ea982816233bdba1bdc4e3e90431bded0244a4b3
Gabor Szabo <gabor@szabgab.com>
2019-04-20 09:42:03 +0300

    new perl example

---
+++ examples/printf.pl
@@ -0,0 +1,8 @@
+
+my $v = 65;
+printf("<%s>\n", $v);     # <65>
+printf("<%10s>\n", $v);   # <      65>
+printf("<%-10s>\n", $v);  # <65      >
+printf("<%c>\n", $v);     # <A>
+printf("<%d>\n", $v);     # <65>
+printf("<%0.5d>\n", $v);  # <00065>
