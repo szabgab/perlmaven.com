a5f6c630b3ae0bd9c1b69f9cac7a6bf590cfe08f
Gabor Szabo <gabor@szabgab.com>
2019-05-06 14:55:50 +0300

    two new posts

---
+++ examples/begin_block_code.pl
@@ -0,0 +1,13 @@
+
+my $z = 1;
+my $q = 23;
+print "DONE\n";
+
+
+BEGIN {
+    # $DB::single = 1;
+    my $welcome = 'Hello World';
+    my $n = 19+23;
+    print "$welcome $n\n";
+}
+
--- examples/fibonacci_with_breakpoint.pl
+++ examples/fibonacci_with_breakpoint.pl
@@ -1,12 +1,17 @@
 use 5.010;
 use strict;
 use warnings;
+no warnings 'once';
+
+my $num = shift or die "Usage: $0 N\n";
+say fibonacci($num);
 
 sub fibonacci {
     my ($n) = @_;
-    say "fibonacci($n)";
+
+    $DB::single = 1 if $n == 6;
+
     return 1 if $n == 1 or $n == 2;
     return fibonacci($n-1) + fibonacci($n-2);
 }
 
-say fibonacci(6);
