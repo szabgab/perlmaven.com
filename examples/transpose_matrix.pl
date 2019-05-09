beff47a7aa9756fd572bb555e589a3fb2e3767f6
Gabor Szabo <gabor@szabgab.com>
2019-05-09 23:20:24 +0300

    update

--- examples/transpose_matrix.pl
+++ examples/transpose_matrix.pl
@@ -3,9 +3,9 @@ use warnings;
 use Data::Dumper qw(Dumper);
 
 my @matrix = (
-    [ 'name11', 'name12', 'name13' ],
-    [ 'name21', 'name22', 'name23' ],
-    [ 'name31', 'name32', 'name33' ],
+    [ 'name11', 'name12', 'name13', 'name14' ],
+    [ 'name21', 'name22', 'name23', 'name24' ],
+    [ 'name31', 'name32', 'name33', 'name34' ],
 );
 
 print Dumper \@matrix;
