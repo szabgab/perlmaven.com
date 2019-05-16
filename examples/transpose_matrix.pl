bdf4f52b53ae250a62f43eb7ff43d05dcf1005e2
Gabor Szabo <gabor@szabgab.com>
2019-05-16 22:13:54 +0300

    Data::Dump in example

--- examples/transpose_matrix.pl
+++ examples/transpose_matrix.pl
@@ -1,6 +1,7 @@
+use 5.010;
 use strict;
 use warnings;
-use Data::Dumper qw(Dumper);
+use Data::Dump qw(dump);
 
 my @matrix = (
     [ 'name11', 'name12', 'name13', 'name14' ],
@@ -8,11 +9,11 @@ my @matrix = (
     [ 'name31', 'name32', 'name33', 'name34' ],
 );
 
-print Dumper \@matrix;
+say dump \@matrix;
 my @tr;
 for my $row (0..@matrix-1) {
     for my $col (0..@{$matrix[$row]}-1) {
         $tr[$col][$row] = $matrix[$row][$col];
     }
 }
-print Dumper \@tr;
+say dump \@tr;
