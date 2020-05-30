b3abcb6f10f663b15795a5d0afa7ccc02e1d0df1
Gabor Szabo <gabor@szabgab.com>
2020-05-30 10:14:50 +0300

    substr

---
+++ examples/substr.pl
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+use 5.010;
+
+my $str = "The black cat climbed the green tree";
+
+say substr $str, 4, 5;                      # black
---
+++ examples/substr_all.pl
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+use 5.010;
+
+my $str = "The black cat climbed the green tree";
+
+say substr $str, 4, 5;      # black
+say substr $str, 22;        # the green tree";
+say substr $str, 22, -5;    # the green           The same as: substr $str, 22, length($str)-22-5;
+say substr $str, -4;        # tree                The same as: substr $str, length($str)-4;
+say substr $str, -10, 5;    # green
+say substr $str, -14, -11;  # the
+
+
+say substr $str, 4, 5, "big";     # black
+say $str;                         # The big cat climbed the green tree
+
+say substr($str, 4, 3) = "small"; # small
+say $str;                         # The small cat climbed the green tree
+
+
