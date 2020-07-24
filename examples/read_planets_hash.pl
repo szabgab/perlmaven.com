93c6ebd31479052dd7e324c9c29ceabd7388da13
Gabor Szabo <gabor@szabgab.com>
2020-07-24 16:22:15 +0300

    shorter lines

--- examples/read_planets_hash.pl
+++ examples/read_planets_hash.pl
@@ -5,7 +5,8 @@ use warnings;
 use Text::CSV;
 use Data::Dumper qw(Dumper);
 
-my $file = $ARGV[0] or die "Need to get CSV file on the command line\n";
+my $file = $ARGV[0]
+    or die "Need to get CSV file on the command line\n";
 
 read_as_hash($file);
 
@@ -15,10 +16,11 @@ sub read_as_hash {
     my $csv = Text::CSV->new ({
         binary    => 1,
         auto_diag => 1,
-        sep_char  => ','    # not really needed as this is the default
+        sep_char  => ',' # not really needed as this is the default
     });
 
-    open(my $data, '<:encoding(utf8)', $filename) or die "Could not open '$filename' $!\n";
+    open(my $data, '<:encoding(utf8)', $filename)
+        or die "Could not open '$filename' $!\n";
     my $header = $csv->getline($data);
     $csv->column_names($header);
     while (my $row = $csv->getline_hr($data)) {
