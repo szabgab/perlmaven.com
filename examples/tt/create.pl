dedeacd99dc9fa509fb0c7a0547c815d5e28eab1
Gabor Szabo <gabor@szabgab.com>
2020-08-19 16:39:45 +0300

    tt example

---
+++ examples/tt/create.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+use Template;
+
+my $tt = Template->new({
+    INCLUDE_PATH => './templates',
+    INTERPOLATE  => 1,
+}) or die "$Template::ERROR\n";
+
+my %data = (
+    title      => 'This is your title',
+    languages  => ['English', 'Spanish', 'Hungarian', 'Henrew'],
+    people     => [
+        { name => 'Foo', email => 'foo@perlmaven.com' },
+        { name => 'Zorg' },
+        { name => 'Bar', email => 'Bar@perlmaven.com' },
+    ],
+);
+$tt->process('report.tt', \%data) or die $tt->error(), "\n";
+
