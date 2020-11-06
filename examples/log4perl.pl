34dcb19d83493cf1f88841cbb64c76f950bfcec9
Gabor Szabo <gabor@szabgab.com>
2020-11-06 17:53:33 +0200

    example

--- examples/log4perl.pl
+++ examples/log4perl.pl
@@ -1,9 +1,11 @@
 use Log::Log4perl ();
 
-#
 Log::Log4perl->easy_init( {
             level    => 'INFO',
             layout   => '%d{yyyy-MM-dd HH:mm:ss} - %p - %F{1}-%L-%M - %m%n',
+            # file     => ">test.log",
+            # utf8     => 1,
+            # category => "Bar::Twix",
 });
 
 my $logger = Log::Log4perl->get_logger();
