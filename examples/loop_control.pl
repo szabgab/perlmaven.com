8bc8aa564c7210b3ce6f5807dcd0ff13a9855b55
Gabor Szabo <gabor@szabgab.com>
2021-01-23 08:49:00 +0200

    add variable to make things clearer

--- examples/loop_control.pl
+++ examples/loop_control.pl
@@ -8,6 +8,7 @@ open my $fh, '<', $file or die;
 
 my $counter = 0;
 my $empty = 0;
+my $nonempty = 0;
 while (my $line = <$fh>) {
     $counter++;
     chomp $line;
@@ -16,14 +17,15 @@ while (my $line = <$fh>) {
     }
 
     if ($line eq "") {
+        $empty++;
         next;     # don't process empty rows, go to next line
     }
 
     print "Process '$line'\n";
-    $empty++;
+    $nonempty++;
                                     ### next jumps here ###
 }
                                     ### last jumps here ###
-print "Number of non empty rows: $empty out of a total of $counter\n"
+print "Number of empty rows $empty and non empty rows: $nonempty out of a total of $counter\n"
 
 
