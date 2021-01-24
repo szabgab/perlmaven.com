d46eeabe9db9d0a963ff4d3f32ed6adf41588955
Gabor Szabo <gabor@szabgab.com>
2021-01-24 18:00:01 +0200

    wrap in main function

--- examples/loop_control.pl
+++ examples/loop_control.pl
@@ -2,30 +2,35 @@
 use strict;
 use warnings;
 
-my $file = shift or die "Usage: $0 FILENAME\n";
+my $filename = shift or die "Usage: $0 FILENAME\n";
+main($filename);
 
-open my $fh, '<', $file or die;
+sub main {
+    my ($file) = @_;
 
-my $counter = 0;
-my $empty = 0;
-my $nonempty = 0;
-while (my $line = <$fh>) {
-    $counter++;
-    chomp $line;
-    if ($line eq "__END__") {
-        last;     # end processing file
+    open my $fh, '<', $file or die;
+
+    my $counter = 0;
+    my $empty = 0;
+    my $nonempty = 0;
+    while (my $line = <$fh>) {
+        $counter++;
+        chomp $line;
+        if ($line eq "__END__") {
+            last;     # end processing file
+        }
+
+        if ($line eq "") {
+            $empty++;
+            next;     # don't process empty rows, go to next line
+        }
+
+        print "Process '$line'\n";
+        $nonempty++;
+                                        ### next jumps here ###
     }
-
-    if ($line eq "") {
-        $empty++;
-        next;     # don't process empty rows, go to next line
-    }
-
-    print "Process '$line'\n";
-    $nonempty++;
-                                    ### next jumps here ###
+                                        ### last jumps here ###
+    print "Number of empty rows $empty and non empty rows: $nonempty out of a total of $counter\n"
 }
-                                    ### last jumps here ###
-print "Number of empty rows $empty and non empty rows: $nonempty out of a total of $counter\n"
 
 
