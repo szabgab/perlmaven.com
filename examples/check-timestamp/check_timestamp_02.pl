fe8f56a9c0922796fb99c5c5d2b343b043f068d5
Gabor Szabo <gabor@szabgab.com>
2021-02-17 22:34:07 +0200

    example

---
+++ examples/check-timestamp/check_timestamp_02.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+die "Usage: $0 FILENAMEs\n" if not @ARGV;
+
+while (my $row = <>) {
+	next if $row !~ /^\d+:/;
+	# 0:00:00.800,0:00:05.500
+	if ($row !~ /^\d+:\d\d:\d\d\.\d\d\d,\d+:\d\d:\d\d\.\d\d\d$/) {
+		warn "timestamp in row $. in $ARGV is incorrectly formatted\n";
+	}
+}
+
