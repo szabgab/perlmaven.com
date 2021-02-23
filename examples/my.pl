912b2a917b83983dae1438146475c67283348002
Gabor Szabo <gabor@szabgab.com>
2021-02-23 20:47:44 +0200

    my

---
+++ examples/my.pl
@@ -0,0 +1,11 @@
+use strict;
+use warnings;
+
+
+my $this_is_global = 23;    # declaring a single scalar and assigning value to it
+my ($x, $y, $z);            # declaring multiple scalars without value, they must be enclosed in parentheses
+
+my ($person, @animals, %data);  # declaring scalar, array, and hash
+
+my ($width, $length) = (23, 29);  # declaring several variables and assigning value at the same time.
+
