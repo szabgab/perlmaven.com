2b41d2e2e9e2a64a1b5677210bea496a167c4e48
Gabor Szabo <gabor@szabgab.com>
2021-03-17 17:25:43 +0200

    chr

---
+++ examples/chr.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+use 5.010;
+
+binmode(STDOUT, 'utf8'); # To avoide the 'Wide character in say' warning.
+
+say chr(65);    # A
+say chr(1488);  # א (Hebrew Aleph)
+say chr(1634);  # ٢ (Arabiv 2)
+say chr(197);   # Å
+say chr(195);   # Ã
+say chr(215);   # ×
+say chr(217);   # Ù
