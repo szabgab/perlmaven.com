d556464594cc2a3d53e5f1965885d112b3493283
Gabor Szabo <gabor@szabgab.com>
2021-03-17 17:27:29 +0200

    typo

--- examples/chr.pl
+++ examples/chr.pl
@@ -6,7 +6,7 @@ binmode(STDOUT, 'utf8'); # To avoide the 'Wide character in say' warning.
 
 say chr(65);    # A
 say chr(1488);  # א (Hebrew Aleph)
-say chr(1634);  # ٢ (Arabiv 2)
+say chr(1634);  # ٢ (Arabic 2)
 say chr(197);   # Å
 say chr(195);   # Ã
 say chr(215);   # ×
