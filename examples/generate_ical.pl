6f796d5da6ccc0783f83d5b5086b36a02412451c
Gabor Szabo <gabor@szabgab.com>
2021-04-05 08:55:43 +0300

    update timestamp

--- examples/generate_ical.pl
+++ examples/generate_ical.pl
@@ -10,31 +10,36 @@ use DateTime::Format::ICal   ();
 use DateTime::Duration       ();
 
 my $calendar = Data::ICal->new;
+my $now = DateTime->now;
 
 {
     my $begin = DateTime->new( year => 2021, month => 4, day => 5, hour => 15 );
     my $end   = DateTime->new( year => 2021, month => 4, day => 5, hour => 17 );
     my $event = Data::ICal::Entry::Event->new;
     $event->add_properties(
-    	summary     => 'Short title of the event',
-    	description => "A longer description that can also have ebedded newlines.\nThen it can continue on a second and\nthird line.",
-    	dtstart     => DateTime::Format::ICal->format_datetime($begin),
-    	location    => 'The local coffee shop',
-    	dtend       => DateTime::Format::ICal->format_datetime($end),
+        summary     => 'Short title of the event',
+        description => "A longer description that can also have ebedded newlines.\nThen it can continue on a second and\nthird line.",
+        dtstart     => DateTime::Format::ICal->format_datetime($begin),
+        location    => 'The local coffee shop',
+        dtend       => DateTime::Format::ICal->format_datetime($end),
+        dtstamp     => DateTime::Format::ICal->format_datetime($now),
+        uid         => DateTime::Format::ICal->format_datetime($begin) . '-short',
     );
     $calendar->add_entry($event);
 }
 
 {
-    my $begin = DateTime->new( year => 2021, month => 4, day => 6, hour => 6 );
+    my $begin = DateTime->new( year => 2021, month => 4, day => 6, hour => 6, time_zone => '+0200' );
     my $duration = DateTime::Duration->new(hours => 3, minutes => 15);
     my $event = Data::ICal::Entry::Event->new;
     $event->add_properties(
-    	summary     => 'Another event',
-    	description => "Description",
-    	dtstart     => DateTime::Format::ICal->format_datetime($begin),
-    	location    => 'https://zoom.us/',
+        summary     => 'Another event',
+        description => "Description",
+        dtstart     => DateTime::Format::ICal->format_datetime($begin),
+        location    => 'https://zoom.us/',
         duration    => DateTime::Format::ICal->format_duration($duration),
+        dtstamp     => DateTime::Format::ICal->format_datetime($now),
+        uid         => DateTime::Format::ICal->format_datetime($begin) . '-other',
     );
     $calendar->add_entry($event);
 }
