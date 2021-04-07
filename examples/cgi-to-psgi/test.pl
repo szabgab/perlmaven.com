e5b4e546d63b89c4c60052ae0b43ae7a0b267c1f
Gabor Szabo <gabor@szabgab.com>
2021-04-07 16:53:42 +0300

    add test

---
+++ examples/cgi-to-psgi/test.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use Test::More;
+use Test::LongString;
+use Capture::Tiny qw(capture);
+
+{
+    my ($out, $err, $exit) = capture { system q{files/var/cgi-bin/app.cgi} };
+    is $exit, 0;
+    is $err, '';
+    is_string $out, "Content-Type: text/html; charset=utf8\r\n\r\nHello \n";
+}
+
+{
+    my ($out, $err, $exit) = capture { system q{files/var/cgi-bin/app.cgi name='Foo Bar'} };
+    is $exit, 0;
+    is $err, '';
+    is_string $out, "Content-Type: text/html; charset=utf8\r\n\r\nHello Foo Bar\n";
+}
+
+
+done_testing;
