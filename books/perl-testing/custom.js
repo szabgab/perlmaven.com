ec9924644e3980048186f0d0f71ba771228d4eb1
Gabor Szabo <gabor@szabgab.com>
2026-03-23 10:59:44 +0100

    add filenames to display

---
+++ books/perl-testing/custom.js
@@ -0,0 +1,4 @@
+const prefixes = ["src/examples/"];
+const extensions = [".pl", ".pm", ".t", ".toml", ".yaml", ".cfg", ".html"];
+const base_url = "https://github.com/szabgab/perlmaven.com/tree/main/books/perl-testing/"
+
