d483f70d1be1a82ea1d78cb8e135d4499ecd9284
Gabor Szabo <gabor@szabgab.com>
2026-04-09 16:56:52 +0200

    links to files

---
+++ books/perl-advanced/custom.js
@@ -0,0 +1,4 @@
+const prefixes = ["src/examples/"];
+const extensions = [".pl", ".pm", ".t", ".toml", ".yaml", ".cfg", ".html", ".PL", ".psgi"];
+const base_url = "https://github.com/szabgab/perlmaven.com/tree/main/books/perl-advanced/"
+
