8d2a2b9237596c7ac46889b01f6452398087732f
Gabor Szabo <gabor@szabgab.com>
2026-04-09 21:30:52 +0200

    caller example

---
+++ books/perl-advanced/src/examples/advanced-perl/caller_more.pl
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+
+f();
+
+sub f {
+    g();
+}
+
+sub g {
+    h();
+}
+
+sub h {
+    for my $i (0..2) {
+        my ($package, $filename, $line, $subroutine, $hasargs, $wantarray, $evaltext, $is_require, $hints, $bitmask, $hinthash) = caller($i);
+        print "$i: $package  $filename  $line $subroutine\n";
+    }
+}
--- books/perl-advanced/src/perl/advanced-perl/caller.md
+++ books/perl-advanced/src/perl/advanced-perl/caller.md
@@ -1,8 +1,10 @@
 # caller
 
-* caller
+* [caller](https://perldoc.perl.org/functions/caller)
 
 {% embed include file="src/examples/advanced-perl/caller.pl" %}
+
+{% embed include file="src/examples/advanced-perl/caller_more.pl" %}
 
 
 
